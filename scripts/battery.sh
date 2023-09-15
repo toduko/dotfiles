0e6d6ad66e10348728235fc6014a2e9d2b44e284
toduko <teddyepich@gmail.com>
2023-09-16 01:01:53 +0300

    Fixed battery scripts

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 BATTERY_INFO_DIRECTORY=/sys/class/power_supply/BAT0
-CAPACITY=$(cat $BATTERY_INFO_DIRECTORY/capacity)
-STATUS=$(cat $BATTERY_INFO_DIRECTORY/status)
 LOW_BATTERY_THRESHHOLD=15 # in percentage
 SLEEP_TIME=300 # in seconds
 
 while true
 do
-  if [[ $CAPACITY < $LOW_BATTERY_THRESHHOLD && $STATUS == "Discharging" ]] ; then
+  CAPACITY=$(cat $BATTERY_INFO_DIRECTORY/capacity)
+  STATUS=$(cat $BATTERY_INFO_DIRECTORY/status)
+
+  if [[ $CAPACITY -lt $LOW_BATTERY_THRESHHOLD && $STATUS == "Discharging" ]] ; then
     DISPLAY=:0.0 /usr/bin/notify-send "LOW BATTERY!!!" "Only $CAPACITY% left."
   fi
 
